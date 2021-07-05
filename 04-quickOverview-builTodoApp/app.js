11ffac21ac8b8414864835ff4a7d47363679c100
LaureanoVera <laureanoigvera@gmail.com>
2021-07-05 10:24:19 -0300

    post request with mongoose

--- 04-quickOverview-builTodoApp/app.js
+++ 04-quickOverview-builTodoApp/app.js
@@ -4,40 +4,52 @@ const Item = require("../models/items");
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({extended: true}));
+
 // database
 const user = "user_items";
 const pass = "k0iiw52UQWbM5wRF";
 const dbname = "item-database";
 const mongodb = `mongodb+srv://${user}:${pass}@cluster0.tc0dq.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 mongoose
-.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => {
-  console.log("DB connected");
-  app.listen(port, () => {
-    console.log(`Server on port ${port}`);
-  });
-})
-.catch((err) => console.log(err));
+  .connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("DB connected");
+    app.listen(port, () => {
+      console.log(`Server on port ${port}`);
+    });
+  })
+  .catch((err) => console.log(err));
 
 // template engine
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  
+  res.redirect("/get-items");
 });
 
 app.get("/get-items", (req, res) => {
-  Item.find().then((result) => {
-    res.send(result)
-    res.render('index', { items: result })
-  }).catch(err => {console.log(err)})
+  Item.find()
+    .then((result) => {
+      res.render("index", { items: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/add-item", (req, res) => {
   res.render("add-item");
 });
 
+app.post('/items', (req, res) => {
+  console.log(req.body);
+  const item = Item(req.body)
+  item.save().then(() => {
+    res.redirect('/get-items')
+  }).catch(err => {console.log(err)})
+})
+
 app.use((req, res) => {
   res.render("error");
 });
-
